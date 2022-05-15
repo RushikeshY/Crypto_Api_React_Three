ec905b823ce090a36c684b36ab82c457206a53fc
RushikeshY <99939510+RushikeshY@users.noreply.github.com>
2022-05-15 18:43:11 +0530

    Pagination update

--- crypto_react_three/src/App.js
+++ crypto_react_three/src/App.js
@@ -2,10 +2,11 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Coin from "./Coin";
+import ReactPaginate from "react-paginate";
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=250&page=1&sparkline=false
 function App() {
   const [coins, setCoins] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   useEffect(() => {
     axios
       .get(
@@ -23,17 +24,38 @@ function App() {
   const handlechange = (e) => {
     setSearch(e.target.value);
   };
-//   console.log(search)
-// console.log(coins)
 
-
-  const filteredCoins = coins.filter((coin)=> 
+  const filteredCoins = coins.filter((coin) =>
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
 
+  const [pageNo, setPageNo] = useState(0);
+  const UserPerPage = 10;
+  const pageVisited = UserPerPage * pageNo;
+  const displayUsers = filteredCoins
+    .slice(pageVisited, pageVisited + UserPerPage)
+    .map((coin) => {
+      return (
+        <Coin
+          key={coin.id}
+          name={coin.name}
+          price={coin.current_price}
+          image={coin.image}
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+        />
+      );
+    });
+
+  const pageCount = Math.ceil(filteredCoins.length / UserPerPage);
+
+  const ChangePage = ({ selected }) => {
+    setPageNo(selected);
+  };
 
   return (
-
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Search a Currency</h1>
@@ -47,22 +69,20 @@ function App() {
         </form>
       </div>
       <div className="coin-div">
-      {filteredCoins.map(coin => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            price={coin.current_price}
-            image={coin.image}
-            
-            symbol={coin.symbol}
-             marketcap={coin.market_cap}
-            priceChange={coin.price_change_percentage_24h}
-            volume={coin.total_volume}
-               
-            />
-        );
-      })}
+        {displayUsers}
+        <div id="page">
+          <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"Next"}
+            pageCount={pageCount}
+            onPageChange={ChangePage}
+            containerClassName={"paginationBtns"}
+            previousLinkClassName={"prevousBtn"}
+            nextLinkClassName={"nextBtn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"PaginationActive"}
+          />
+        </div>
       </div>
     </div>
   );
