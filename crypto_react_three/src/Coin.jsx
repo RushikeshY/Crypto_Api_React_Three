eb9f9a1d29522d464ee4d147962fb6a612e8b00a
RushikeshY <99939510+RushikeshY@users.noreply.github.com>
2022-05-20 22:43:26 +0530

    update

--- crypto_react_three/src/Coin.jsx
+++ crypto_react_three/src/Coin.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import "./Coin.css"
-const Coin = ({name,image,symbol,price,volume,priceChange,marketcap}) => {
-//    console.log(price)
-    return (
+import React from "react";
+import "./Coin.css";
+const Coin = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketcap,
+}) => {
+  //    console.log(price)
+  return (
     <div className="coin-container">
-        <div className="coin-row">
-            <div className="coin">
-               <img src={image} alt="crypto" className='coin-img' />
-               <h1 className='coin-name'>{name}</h1>
-               <p className="coin-symbol">{symbol}</p>
-            </div>
-            <div className="coin-data">
-                <p className="coin-price"> ₹{price}</p>
-                <p className='coin-volume'>{volume.toLocaleString() }</p>
-                 
-                 {
-                     priceChange<0 ? (
-                         <p className='coin-percent red'>{priceChange.toFixed(2)} %</p>
-
-                     ):  (<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>)
-
-                 }
-
-                <p className="coin-marketcap">
-                    Mkt Cap; ${marketcap.toLocaleString()}
-                </p>
-                
-            </div>
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" className="coin-img" />
+          <h1 className="coin-name">{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
         </div>
+        <div className="coin-data">
+          <p className="coin-price"> ₹{price}</p>
+          <p className="coin-volume">{volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)} %</p>
+          ) : (
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className="coin-marketcap">
+            Mkt Cap; ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Coin;
